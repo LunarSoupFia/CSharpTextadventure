ad81da52fdc08ec82d6ff384181bf6c205c9579f
Cowan Fischer <Cowanqwe@outlook.de>
2022-04-13 20:09:24 +0200

    added half of the interface

--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,34 @@ namespace Textadventure
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
