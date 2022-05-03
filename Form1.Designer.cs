f7683087e61073dae46879356a03abdac5d23573
Cowan Christian Gawain <ABEU@bsbretten.onmicrosoft.com>
2022-05-03 15:15:56 +0200

    Added VillageQuest1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,11 @@ namespace Textadventure
             this.action4 = new System.Windows.Forms.Label();
             this.action5 = new System.Windows.Forms.Label();
             this.action6 = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // outputBox
@@ -193,6 +198,61 @@ namespace Textadventure
             this.action6.Text = "drücke";
             this.action6.Click += new System.EventHandler(this.action6_Click);
             // 
+            // option1
+            // 
+            this.option1.AutoSize = true;
+            this.option1.BackColor = System.Drawing.Color.Transparent;
+            this.option1.ForeColor = System.Drawing.Color.White;
+            this.option1.Location = new System.Drawing.Point(23, 919);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(54, 32);
+            this.option1.TabIndex = 12;
+            this.option1.Text = "____";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(83, 919);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "____";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(153, 919);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "____";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(213, 919);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 32);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "____";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(273, 919);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 32);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "____";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
@@ -201,6 +261,11 @@ namespace Textadventure
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1485, 960);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.option1);
             this.Controls.Add(this.action6);
             this.Controls.Add(this.action5);
             this.Controls.Add(this.action4);
@@ -236,6 +301,11 @@ namespace Textadventure
         private System.Windows.Forms.Label action4;
         private System.Windows.Forms.Label action5;
         private System.Windows.Forms.Label action6;
+        private System.Windows.Forms.Label option1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
---
+++ VillageQuest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textadventure
+{
+    internal class VillageQuest1
+    {
+        private static string nl = Environment.NewLine;
+        
+        private static Scene[] _scenes = {
+            new Scene(new Option[]
+            {   new Option(OptionIdentifier.ASK, "Du fragst die Frau nach ihrem Kind, sie beginnt dir von ihm zu erzählen" + nl + "Mein Sohn ist ein echter Rabauke, aber er verschwindet manchmal für Stunden, ohne dass ich weiß wo er hin geht." + nl + "Bitte findet ihn so schnell wie möglich!", new string[]{"DORF_QUEST1_SUCHE"}, new string[]{"Kind", "Aufenthaltsort", "Wo", "Sohn"}),
+                new Option(OptionIdentifier.ASK, "Du bietest der Mutter deine Hilfe an. Sie bedankt sich und gibt dir eine Info zu ihrem Sohn:" + nl + "Er versteckt sich bestimmt hier im Dorf", new string[]{"DORF_QUEST1_SUCHE"}, new string[]{"Hilfe", "Helfen", "anbieten", "Unterstützung"}),
+                new Option(OptionIdentifier.GO, "Die Anliegen der Frau sind für dich zum jetzigen Zeitpunkt nicht von Bedeutung und du entfernst dich von ihr.", new string[]{"DORF1"}, new string[]{"weg", "Dorf", "zurück", "Dorfplatz", "Stadt"})
+            
+            }, "DORF_QUEST1", "Eine junge Frau kommt auf dich zu. Sie scheint sehr besorgt:" + nl + "Bitte, ihr seid doch ein Abenteurer, mein Sohn ist verschwunden und ich kann ihn nicht finden!"),
+            new Scene(new Option[]
+            {   new Option(OptionIdentifier.LOOK, "Als du hinter den Fässern schaust, findest du #den vermissten Jungen. Du bringst ihn zu seiner Mutter. #einige Ratten, die ein Bankett mit Tee und diversen Gerichten halten. Die Ratten sind von deinem Verhalten empört und bitten dich höflich zu gehen.", new string[]{"DORF_QUEST1_END", "DORF_QUEST1_SUCHE_LF"}, new string[]{"Fässer", "Fässern", "Fass", "Rumpern"}),
+                new Option(OptionIdentifier.OPEN, "Als du in die Fässer schaust, siehst du #den vermissten Jungen. Nachdem du ihn aus einem nassen Fass ziehst bringst du den Jungen zu seiner Mutter. #nur Wein, Obst und Gemüse.", new string[]{"DORF_QUEST1_END", "DORF_QUEST1_SUCHE_OF"}, new string[]{"Fässer", "Fässern", "Fass", "Rumpern"}),
+                new Option(OptionIdentifier.LOOK, "Als du dich in der Schmiede umschaust, findest du #den vermissten Jungen, welcher mit den Hämmern und Zangen spielt. Nachdem du ihm die Schmiedewerkzeuge abgenommen hast bringst du den Jungen zu seiner Mutter. #einige Vögel, welche sich im Innendach der Schmiede ein Nest gebaut haben. Sie scheinen kein Problem darzustellen.", new string[]{"DORF_QUEST1_END", "DORF_QUEST1_SUCHE_LS" }, new string[]{"Schmied", "Schmiede", "metallene", "Metall", "Klänge"})
+            
+            }, "DORF_QUEST1_SUCHE", "Du suchst in der Stadt nach dem Kind. Hinter den Fässern hörst du ein Rumpern. In der Schmiede hörst du einige metallene Klänge, die nicht vom Schmied kommen können."),
+            new Scene(new Option[]
+            {   new Option(OptionIdentifier.GO, "Du bedankst dich für das Angebot und nimmst am Essen teil. Als du ein von der Bäckerin selbstgebackenes Brot aufschneidest, fällt dir der Wandteppich auf, auf dem ein Tempel mit einigen Symbolen gestickt ist. nach kurzem Nachfragen erzählt dir die Frau, dass es sich um einen uraltes Familienerbstück handelt, dessen Bedeutung sie nicht kennt. Am unteren Rand sind 5 Symbole angedeutet, von denen das erste und dritte komplett verblasst sind. Die erkennbaren Symbole sind:"
+                                                + nl + "ein Mond, ein Stern und ein weiterer Mond." + nl + "Am nächsten Morgen hast du dir die Symbole gut eingeprägt.", new string[]{"DORF1"}, new string[]{"Haus", "Bäckerei", "Bäckerhaus", "Mutter", "Frau", "Bäckerin"}),
+                new Option(OptionIdentifier.GO, "Du lehnst dankend ab, du hättest leider noch einiges zu tun", new string[]{"DORF1"}, new string[]{"weg", "Dorf", "zurück", "Dorfplatz", "Stadt"})
+            }, "DORF_QUEST1_END", "Du bringst der Mutter ihr Kind. Voller Erleichterung bedankt sie sich bei dir, und lädt dich zum Essen bei ihr im Bäckerhaus ein.")
+        };
+    }
+}
