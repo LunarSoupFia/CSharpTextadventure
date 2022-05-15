c2812626872acf6a0ede779620948455cef505be
Cowan Christian Gawain <ABEU@bsbretten.onmicrosoft.com>
2022-05-15 17:50:09 +0200

    fixed a bug in the Quest and QuestLog classes.

--- Quest.cs
+++ Quest.cs
@@ -5,35 +5,34 @@
 /// </summary>
 public class Quest
 {
-	public Quest()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
+	
 		public string QuestName
 		{
 			get { return QuestName; }
+			set { QuestName = value; }
 		}
 		public string QuestDescription
 		{
 			get { return QuestDescription; }
+			set { QuestDescription = value; }
 		}
 		public string EndSceneIdentifier
 		{
 			get { return EndSceneIdentifier; }
+			set { EndSceneIdentifier = value; }
 		}
 		public string QuestReward
 		{
 			get { return QuestReward; }
+			set { QuestReward = value; }
 		}
 
 		public Quest(string Name, string Description, string Identifier, string Reward)
-    {
+		{
 		this.QuestName = Name;
 		this.QuestDescription = Description;	
 		this.EndSceneIdentifier = Identifier;
 		this.QuestReward = Reward;
-    }
+		}
 
-	}
 }
--- QuestLog.cs
+++ QuestLog.cs
@@ -5,13 +5,10 @@
 /// </summary>
 public class QuestLog
 {
-	public QuestLog()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
 	private static string nl = Environment.NewLine;
-	private static Scene[] _scenes = {
-			new Scene(new Option[]{
-}
+	public QuestLog()
+	{ }
+		//
+		// TODO: Add Quest Log logic here
+		//
 }
