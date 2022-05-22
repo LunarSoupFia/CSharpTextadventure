0a949b0651d6f28079f01c8ca729cc1821755f54
Aaron <Aaron@TABLET-STIF5VNL>
2022-05-22 17:19:56 +0200

    added Questlog

--- QuestLog.cs
+++ QuestLog.cs
@@ -5,37 +5,37 @@
 /// </summary>
 public class QuestLog
 {
-	private static string nl = Environment.NewLine;
-	//
-	// TODO: Add Quest Log logic here
-	//
-	private string _questLogTitle;
+    private static string nl = Environment.NewLine;
+    //
+    // TODO: Add Quest Log logic here
+    //
+    private string _questLogTitle;
 
-	public string QuestLogTitle
-	{
-		get { return _questLogTitle; }
-		set { _questLogTitle = value; }
-	}
+    public string QuestLogTitle
+    {
+        get { return _questLogTitle; }
+        set { _questLogTitle = value; }
+    }
 
-	private string _questLogDescription;
+    private string _questLogDescription;
 
-	public string QuestLogDescription
-	{
-		get { return _questLogDescription; }
-		set { _questLogDescription = value; }
-	}
+    public string QuestLogDescription
+    {
+        get { return _questLogDescription; }
+        set { _questLogDescription = value; }
+    }
 
-	private Quest[] _quests;
-	public Quest[] Quests
-	{
-		get { return _quests; }
-		set { _quests = value; }
-	}
+    private Quest[] _quests;
+    public Quest[] Quests
+    {
+        get { return _quests; }
+        set { _quests = value; }
+    }
 
-	public QuestLog(string QuestLogTitle, string QuestLogDescription, Quest[] Quests)
-	{ 
-		this.QuestLogTitle = QuestLogTitle;
-		this.QuestLogDescription = QuestLogDescription;
-		this.Quests = Quests;
-	}
+    public QuestLog(string QuestLogTitle, string QuestLogDescription, Quest[] Quests)
+    {
+        this.QuestLogTitle = QuestLogTitle;
+        this.QuestLogDescription = QuestLogDescription;
+        this.Quests = Quests;
+    }
 }
